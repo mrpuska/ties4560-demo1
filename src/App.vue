77d5e9b4ec49f4d62b80f927b4084098eb1b877f
xentare <H4113@student.jamk.fi>
2022-09-12 22:26:37 +0300

    Add demo 1.1

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
 import Main from './components/Main.vue'
+import LunchApp from './components/LunchApp.vue'
+import {ref} from 'vue'
+
+  var app = ref(false);
+
 </script>
 
 <template>
-  <div>
-    <h1>IP Country fetcher</h1>
+  <div class="app">
+    <div class="links">
+      <a @click="app = false">IP Country fetcher</a>
+      <a @click="app = true">Lunch App spec</a>
+    </div>
     <div class="wrapper">
       <suspense>
-        <Main/>
+        <Main v-if="!app" />
       </suspense>
     </div>
+      <LunchApp v-if="app"/>
   </div>
 </template>
 
 <style scoped>
-
+a {
+  font-size: xx-large;
+  cursor: pointer;
+}
+.links a{
+  margin-right: 0.5em;
+}
+.app {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/components/LunchApp.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>Here is a model presentation of an application for viewing restaurants and representing their lunch menus.</p>
+  <p>The application is supposed to have a web ui frontend, which is not presented here.</p>
+  <br>
+  <p>The application is fairly simple, building mainly from a REST api, data access layer and an integration service
+    for fetching the restaurant information and all of the lunch menus. On the right side, simple use cases for the 
+    user and an administrator user can be found.</p>
+  <img src="@/assets/lunch-app.drawio.png">
+</template>
+
+<style scoped>
+:host {
+  display: block;
+}
+</style>
